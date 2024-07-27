54b25b8c33cd526afae613912fcb52540b9747ca
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2024-07-28 02:16:13 +0500

    Hide skill bars

--- src/portfolio.js
+++ src/portfolio.js
@@ -148,7 +148,7 @@ const educationInfo = {
 // Your top 3 proficient stacks/tech experience
 
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
+  viewSkillBars: false, //Set it to true to show Proficiency Section
   experience: [
     {
       Stack: "Frontend/Design", //Insert stack or technology you have experience in
